7e098a22d1cb8b0b46278349616ab6e54568b7a8
tomi-casabona <tomas.casabona@gmail.com>
2024-03-14 17:50:31 +0100

    Ejercicio 1.6 Arrays & loops 1-6

--- script.js
+++ script.js
@@ -345,3 +345,47 @@ console.log("Exercici 1.5: Array transformations 6");
 let arrayE =  [11, 12, 13, 14];
 console.log("some > 10 ? " + arrayE.some(n => n>10));
 console.log("every < 10 ? " + arrayE.some(n => n>10));
+
+// Exercici 1
+// forEach: Teniu una array de noms. Utilitza forEach per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
+console.log("Exercici 1.5: Array loops 1 ");
+let noms = ['Anna', 'Bernat', 'Clara']
+noms.forEach(n => console.log(n));
+// Exercici 2
+// for-of: Teniu una array de noms. Utilitza un bucle for-of per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
+console.log("Exercici 1.5: Array loops 2 ");
+for (const nom of noms) {
+  console.log(nom);
+}
+
+// Exercici 3
+// filter: Teniu una array de números. Utilitza filter per a crear una nova array que només contingui els números parells. let numeros = [1, 2, 3, 4, 5, 6];
+console.log("Exercici 1.5: Array loops 3");
+let numeros = [1, 2, 3, 4, 5, 6];
+console.log(numeros.filter(n => n%2===0));
+// Exercici 4
+// for-in: Teniu un objecte amb parells clau-valor: let obj = { nom: Ona, edat: 25, ciutat: 'Barcelona' }; Utilitza un bucle for-in per a imprimir a la consola cada clau i el seu valor corresponent.
+console.log("Exercici 1.5: Array loops 4");
+let obj = { nom: "Ona", edat: 25, ciutat: 'Barcelona' };
+for (const key in obj) {
+ console.log(key +" : "+obj[key]);
+}
+// Exercici 5
+// for-of amb break: Teniu una array de números. Utilitza un bucle for-of per a imprimir a la consola els números fins a trobar el número 5, llavors atura el bucle: let numeros = [1, 2, 3, 4, 5, 6];
+console.log("Exercici 1.5: Array loops 5");
+
+for (const n of numeros) {
+  if(n<5){
+console.log(n)
+  }else {break;}
+}
+
+// Nivell 3
+// Exercici 6
+// for-of amb index: Utilitza un bucle for-of per a imprimir a la consola cada element de l'array i la seva posició (index): let noms = ['Anna', 'Bernat', 'Clara']
+let nombres = ['Anna', 'Bernat', 'Clara'];
+for (let [index, n] of nombres) {
+  console.log("index: "+ index + " nombre: "+ n)
+};
+
+
